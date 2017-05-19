4637f61b6cf3b323349b885cef2421942d86d6ba
yechuzheng <YCZ>
2017-05-19 15:44:17 +0800

    -

--- src/app/home/test/swfObject/swfObject.component.ts
+++ src/app/home/test/swfObject/swfObject.component.ts
@@ -5,6 +5,7 @@
 import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
 import {Message} from "primeng/components/common/api";
 import * as swfObject from 'swfObject';
+import {Source} from "../../../shared/models/source";
 
 declare var $: any;
 @Component({
@@ -15,9 +16,22 @@ declare var $: any;
 export class SwfObjectComponent implements OnInit{
     msgs: Message[] = [];
     @ViewChild('videoPlay') videoPlay: any;
+    sources:Source[];
+    Source:Source = new Source();
     constructor() {}
 
     ngOnInit() {
+        this.sources = [
+            {"id":1,"name":"直播01","createTime":"2017-03-26 12:12:22","photo":"http://angular2.axuer.com/resources/images/logos/angular2/angular.svg","path":"http://iot1.hzcwtech.com/hls/demo1004.m3u8","type":1},
+            {"id":2,"name":"直播02","createTime":"2017-03-26 12:12:22","photo":"http://angular2.axuer.com/resources/images/logos/angular2/angular.svg","path":"http://stjy.bjqjyj.cn/hls/330108001_187.m3u8","type":1},
+            {"id":3,"name":"直播03","createTime":"2017-03-27 12:12:22","photo":"http://angular2.axuer.com/resources/images/logos/angular2/angular.svg","path":"http://stjy.bjqjyj.cn/hls/330108001_186.m3u8","type":1},
+        ];
+        this.Source = this.sources[0];
+        this.onPlayer();
+    }
+    onClickStream(Source){
+        console.info(swfObject.getFlashPlayerVersion());
+        this.Source = Source;
         this.onPlayer();
     }
 
@@ -27,12 +41,12 @@ export class SwfObjectComponent implements OnInit{
         let swfVersionStr = "10.0.0";
         let xiSwfUrlStr = "/assets/swfObject/expressInstall.swf";
         let flashvarsObj = {
-            src: "http://stjy.bjqjyj.cn/hls/330108001_187.m3u8",
+            src: this.Source.path,
             autoPlay: "false",
             verbose: "true",
             controlBarAutoHide: "true",
             controlBarPosition: "bottom",
-            poster: "/assets/images/dq.jpg",
+            poster: "/assets/images/dq.png",
             // plugin_hls: "/assets/player/plugin/expressInstall.swf"
             plugin_hls: "/assets/swfObject/HLSDynamicPlugin.swf"
         };
